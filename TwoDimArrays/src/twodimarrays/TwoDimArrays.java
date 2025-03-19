c3593fdcea4c3847f3c54fcc6da7c85d780143c1
iamgiovannivera <160366865+iamgiovannivera@users.noreply.github.com>
2025-03-19 12:30:50 -0500

    ate

---
+++ TwoDimArrays/src/twodimarrays/TwoDimArrays.java
@@ -0,0 +1,20 @@
+package twodimarrays;
+
+public class TwoDimArrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x[][]={{10,11,12,13,14}, {20,21,22,23,24}, {30,31,32,33,34}, {40,41,41,43,44}};
+        for(int r=0; r<4; r++)
+        {
+            for(int c=0; c<5; c++)
+            {
+                System.out.print(x[r][c]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
